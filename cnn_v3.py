d5f599a7a4ccb6d347d20aee63d3c36d26a90c5f
Jianing Sun <jianing.sun@mail.mcgill.ca>
2017-12-09 22:27:46 -0500

    Update cnn_v3.py

--- cnn_v3.py
+++ cnn_v3.py
@@ -8,15 +8,16 @@ import os
 import torch.nn.functional as F
 import numpy as np
 import pdb
+import copy
 
 # LOADING DATASET
 
-train_dataset = dsets.MNIST(root='./data',
+train_dataset = dsets.CIFAR10(root='./data',
                             train=True,
                             transform=transforms.ToTensor(),
                             download=True)
 
-test_dataset = dsets.MNIST(root='./data',
+test_dataset = dsets.CIFAR10(root='./data',
                            train=False,
                            transform=transforms.ToTensor())
 
@@ -27,7 +28,7 @@ test_dataset = dsets.MNIST(root='./data',
 # print(test_dataset.test_labels.size())   #torch.Size([10000])
 
 batch_size = 100
-num_iter = 3000
+num_iter = 10 ** 5
 num_epoches = num_iter / (len(train_dataset) / 100)  # 5 epoches
 num_epoches = int(num_epoches)
 
@@ -47,7 +48,7 @@ class CNNModel(nn.Module):
     def __init__(self):
         super(CNNModel, self).__init__()
         # Convolution Layer 1 + VALID PADDING
-        self.cnn1 = nn.Conv2d(in_channels=1, out_channels=16, kernel_size=5, stride=1, padding=0)
+        self.cnn1 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=5, stride=1, padding=0)
         self.relu1 = nn.ReLU()
         # Max Pooling 1
         self.maxpool1 = nn.MaxPool2d(kernel_size=2)
@@ -57,7 +58,7 @@ class CNNModel(nn.Module):
         # Max Pooling 2
         self.maxpool2 = nn.MaxPool2d(kernel_size=2)
         # Fully Connected Layer (readout)
-        self.fc1 = nn.Linear(32 * 4 * 4, 10)
+        self.fc1 = nn.Linear(32 * 5 * 5, 10)
 
     def forward(self, x):
         out = self.cnn1(x)
@@ -90,8 +91,8 @@ class LinearSimple(nn.Module):
 
 
 class controlledConv2(nn.Module):
-    def __init__(self, conv, ControlType='linear', bias=None, rnk_ratio=.5):
-        super(controlledConv2, self).__init__()
+    def __init__(self, conv, ControlType='linear', bias=None, rnk_ratio=.5, *args, **kwargs):
+        super(controlledConv2, self).__init__(*args, **kwargs)
         # print('controlledConv2')
         self.conv = conv
         s = conv.weight.size()  # Copy the weights as a constant from the original convolution
@@ -108,15 +109,20 @@ class controlledConv2(nn.Module):
         s = conv.weight.size()
         # if ControlType == 'linear':
         #     ctrl = LinearSimple(s)
-        # self.ctrl = LinearSimple(s)
+        # self.ctrl = ctrl
         self.P = Parameter(torch.eye(s[0]) + torch.eye(s[0]) * torch.randn(s[0], s[0]) / s[0])
         self.register_parameter('P', self.P)
         # print(self.P)
+
         # bias
         # s_bias = self.s[0]
         # self.conv_bias = Variable(torch.Tensor(conv.bias.data.size()).copy_(conv.bias.data))
         # self.conv_bias = self.conv_bias.detach().cuda()
         # self.bias.data.copy_(conv.bias.data[:s_bias])
+
+    def setConvLearnable(self, T):
+        for p in self.conv.parameters():
+            p.requires_grad = T
 
     def set_bn(self, bn):
         my_bn = nn.BatchNorm2d(bn.num_features, affine=bn.affine)
@@ -126,33 +132,35 @@ class controlledConv2(nn.Module):
         self.my_bn = my_bn
         self.old_bn = bn
 
-    def forward(self, x, alpha=None):
+    def forward(self, input, alpha=None):
         # Modify the weights
         # print('forward')
         s = self.s
         w = self.w
-        # print('w',w)
-        newW = torch.matmul(self.P, w)#.cuda()
+        # print('w', w)
+        newW = torch.matmul(self.P, w)  # .cuda()
         # newW = w
+        # newW = self.ctrl(w)
         # print('newW2', newW)
 
         if alpha is not None:
             # print 'got alpha'
             alpha1 = alpha.expand_as(w)
             newWeights = alpha1 * newW + (1 - alpha1) * w
-
+            # print(alpha1)
             alpha2 = alpha.squeeze().expand_as(self.bias)
             # bias = alpha2 * self.bias + (1 - alpha2) * self.conv_bias
         else:
-            # print 'no alpha'
+            # print('no alpha')
             newWeights = newW
             # bias = self.bias
         newWeights = newWeights.contiguous()  # unnecessary
         newWeights = newWeights.view(s)
 
-        x = F.conv2d(x, newWeights, bias=None, stride=self.conv.stride,
+        # print(self.conv.stride, self.conv.dilation, self.conv.padding)
+        x = F.conv2d(input, newWeights, bias=None, stride=self.conv.stride,
                      padding=self.conv.padding, dilation=self.conv.dilation)
-        # print(type(x))
+        # print('x \n', x)
         # apply the batch normalization...
         if self.my_bn is not None:
             x_bn = self.my_bn(x)
@@ -161,7 +169,48 @@ class controlledConv2(nn.Module):
                 x = alpha3 * x_bn + (1 - alpha3) * self.old_bn(x)
             else:
                 x = x_bn
+        # print(x)
         return x
+
+    # def make_layer(self):
+    #     conv_layer = self.forward()
+    #     self.cnn1 = conv_layer(in_channels=3, out_channels=16, kernel_size=5, stride=1, padding=0)
+    #     self.relu1 = nn.ReLU()
+    #     # Max pool 1
+    #     self.maxpool1 = nn.MaxPool2d(kernel_size=2)
+    #     # Convolution 2
+    #     self.cnn2 = conv_layer(in_channels=16, out_channels=32, kernel_size=5, stride=1, padding=0)
+    #     self.relu2 = nn.ReLU()
+    #     # Max pool 2
+    #     self.maxpool2 = nn.MaxPool2d(kernel_size=2)
+    #     self.fc1 = nn.Linear(32 * 5 * 5, 10)
+    #
+    # def forward_2(self, x):
+    #     # Convolution 1
+    #     print('here')
+    #     out = self.cnn1(x)
+    #     out = self.relu1(out)
+    #
+    #     # Max pool 1
+    #     out = self.maxpool1(out)
+    #
+    #     # Convolution 2
+    #     out = self.cnn2(out)
+    #     out = self.relu2(out)
+    #
+    #     # Max pool 2
+    #     out = self.maxpool2(out)
+    #
+    #     # Resize
+    #     # Original size: (100, 32, 7, 7)
+    #     # out.size(0): 100
+    #     # New out size: (100, 32*7*7)
+    #     out = out.view(out.size(0), -1)
+    #
+    #     # Linear function (readout)
+    #     out = self.fc1(out)
+    #
+    #     return out
 
 
 def makeItControlled(origModule, newModule, controlAnyway=True, ControlType='linear', rnk_ratio=.5, verbose=False):
@@ -186,40 +235,82 @@ def makeItControlled(origModule, newModule, controlAnyway=True, ControlType='lin
                 params_after = 2 * (O * rnk_ratio) ** 2
 
             if params_after < params_before or controlAnyway:
-                m = controlledConv2(module1, ControlType, bias=None, rnk_ratio=0.5)  # important!!
-                # print(m)
+                m = controlledConv2(module1, ControlType, bias=None, rnk_ratio=0.5)  # only this line is important
+                # print('m \n', m)
                 setattr(newModule, name1, m)
 
         makeItControlled(module1, module2, controlAnyway=controlAnyway,
                          ControlType=ControlType, verbose=False, rnk_ratio=rnk_ratio)
+    return newModule
 
 
-import copy
-
-model = CNNModel()
-newmodel = copy.deepcopy(model)
-print('before being controlled', newmodel)
+model = CNNModel()  # 2-layer CNN model
+# newmodel = copy.deepcopy(model)
+newmodel = model
+# print('before being controlled: \n', newmodel)
 if torch.cuda.is_available():
     model.cuda()
+print('model', model)
+newmodel = makeItControlled(model, newmodel, controlAnyway=True, ControlType='linear', rnk_ratio=.5, verbose=False)
 
-makeItControlled(model, newmodel, controlAnyway=True, ControlType='linear', rnk_ratio=.5, verbose=False)
 if torch.cuda.is_available():
     newmodel.cuda()
-print('new model with control module', newmodel)
+
+print('newmodel', newmodel)
+# print('new model with control module: \n', newmodel)
+
+
+# class CNNModel2(nn.Module):
+#     def __init__(self):
+#         super(CNNModel2, self).__init__()
+#         conv_layer = controlledConv2(model)
+#         # Convolution Layer 1 + VALID PADDING
+#         self.cnn1 = conv_layer(in_channels=1, out_channels=16, kernel_size=5, stride=1, padding=0)
+#         self.relu1 = nn.ReLU()
+#         # Max Pooling 1
+#         self.maxpool1 = nn.MaxPool2d(kernel_size=2)
+#         # Convolution Layer 2 + VALID PADDING
+#         self.cnn2 = conv_layer(in_channels=16, out_channels=32, kernel_size=5, stride=1, padding=0)
+#         self.relu2 = nn.ReLU()
+#         # Max Pooling 2
+#         self.maxpool2 = nn.MaxPool2d(kernel_size=2)
+#         # Fully Connected Layer (readout)
+#         self.fc1 = nn.Linear(32 * 4 * 4, 10)
+#
+#     def forward(self, x):
+#         out = self.cnn1(x)
+#         out = self.relu1(out)
+#         out = self.maxpool1(out)
+#
+#         out = self.cnn2(out)
+#         out = self.relu2(out)
+#         out = self.maxpool2(out)
+#
+#         # flatten
+#         # Original size: (100,32,4,4)
+#         # Out.size(0): 100
+#         # New out size: (100,32*4*4)
+#         out = out.view(out.size(0), -1)
+#         # Linear function for readout
+#         out = self.fc1(out)
+#
+#         return out
+
+# newmodel = CNNModel2()
+# newmodel.cuda()
+
 
 # INSTANTIATE LOSS CLASS
 criterion = nn.CrossEntropyLoss()
-
 # INSTANTIATE OPTIMIZER CLASS
-
-learning_rate = 0.01  # TODO: here learning rate is fixed, so need to find out some methods, maybe not fixed?
+learning_rate = 0.1  # TODO: here learning rate is fixed, so need to find out some methods, maybe not fixed?
 optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 # criterion = nn.CrossEntropyLoss()
 # optimizer = torch.optim.Adam(model.parameters())
 # print(help(torch.max))
 # print(help(format))
-# TRAIN THE MODEL
 
+# TRAIN THE MODEL;'
 iter = 0
 for epoch in range(num_epoches):
     for i, (images, labels) in enumerate(train_loader):
@@ -231,14 +322,14 @@ for epoch in range(num_epoches):
             labels = Variable(labels)
 
         optimizer.zero_grad()  # Clear gradients first
-        outputs = newmodel(images)  # Forward to get outputs
-        loss = criterion(outputs, labels)  # Cross-entry loss function
+        outputs = model(images)  # Forward to get outputs
+        loss = criterion(outputs, labels)  # Cross-entropy loss function
         loss.backward()
         optimizer.step()
 
         iter += 1
 
-        if iter % 500 == 0:  # Calcualte Accuracy
+        if iter % 500 == 0:  # Calculate Accuracy
             correct = 0
             total = 0
             for images, labels in test_loader:
@@ -247,7 +338,7 @@ for epoch in range(num_epoches):
                 else:
                     images = Variable(images)
 
-                outputs = newmodel(images)
+                outputs = model(images)
                 predicted = torch.max(outputs.data, 1)[1]  # Get predictions from  the maximum value.
                 # function:: max(input, dim, keepdim=False, out=None) -> (Tensor, LongTensor)
                 # Returns the maximum value of each row of the :attr:`input` Tensor in the given
